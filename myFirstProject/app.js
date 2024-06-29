9d1afc4f03b62c74a5fa809dbf8977501cea445b
Shariq Ikram <shariqikram0@gmail.com>
2024-06-29 17:01:21 +0500

    Add files via upload

---
+++ myFirstProject/app.js
@@ -0,0 +1,5 @@
+export const Heading = ()=>{
+    return(
+      <h1>Hello React!</h1>
+    )
+  }
---
+++ myFirstProject/src/App.jsx
@@ -0,0 +1,19 @@
+import React from "react";
+
+
+const App = ()=>{
+  return(
+      <div>
+          <h1>welcome to My web Page.</h1>
+          <p>My Name is Muhammad shariq</p>
+          <img src="/public/coding.png" alt="coding " />
+        
+          
+      </div>
+  )
+}
+
+
+
+
+export  {App}
